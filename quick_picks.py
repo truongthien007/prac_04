6d50c53a4d5103160784fd35c142b0aa5ff41eb8
Dan <118102655+truongthien007@users.noreply.github.com>
2022-12-04 17:19:41 +0700

    not repeated number

--- quick_picks.py
+++ quick_picks.py
@@ -3,9 +3,8 @@ import random
 quick_pick = int(input("How many quick pick you want to generate? "))
 for i in range(0, quick_pick):
     values = []
-    for j in range(0, 6):
+    while len(values) != 6:
         value = random.randint(1, 45)
         if value not in values:
-            values.append(value)
-        print(value, end=" ")
-    print()
+            values.append(str(value))
+    print(', '.join(values))
